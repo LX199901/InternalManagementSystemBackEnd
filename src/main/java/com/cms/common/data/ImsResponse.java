c73cbb2005f419fc851507ae10e1bcd66ec59fdb
LX199901 <167851178+LX199901@users.noreply.github.com>
2024-07-30 18:34:16 +0900

    2024年7月30日18:34:14

--- src/main/java/com/cms/common/data/ImsResponse.java
+++ src/main/java/com/cms/common/data/ImsResponse.java
@@ -7,6 +7,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class ImsResponse<T> {
     private String message;
     private boolean status;
+    private T data;
+    
     private List<T> results;
 
     public ImsResponse(boolean status, String message) {
@@ -21,6 +23,10 @@ public class ImsResponse<T> {
     	this.status = true;
     	this.message = "";
         this.results = results;
+    }
+    
+    public ImsResponse(T data) {
+        this.data = data;
     }
     
     @JsonProperty
@@ -44,5 +50,10 @@ public class ImsResponse<T> {
     @JsonProperty
     public List<T> getResults() {
         return this.results;
+    }
+    
+    @JsonProperty
+    public T getData() {
+        return this.data;
     }
 }
